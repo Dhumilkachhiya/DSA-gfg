b4f9860f1646f90633cb3fd7e9bf1c562d64e4f0
Dhumil Kachhiya <140485981+Dhumilkachhiya@users.noreply.github.com>
2026-07-14 12:09:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Smallest Non-Zero Number/smallest-non-zero-number.cpp
+++ Difficulty: Medium/Smallest Non-Zero Number/smallest-non-zero-number.cpp
@@ -1,28 +1,29 @@
 class Solution {
 	public:
-	bool check(long long energy, vector<int>& arr) {
-		const long long LIMIT = 1e18;
+	bool check(long long energy, vector<int>& arr, int high) {
 		
 		for (int h : arr) {
-			energy = 2LL * energy - h;
+			energy = 2*energy - h;
 			
 			if (energy < 0)
 				return false;
+			if (energy >= high)
+				return true;
 			
-			if (energy > LIMIT)
-				energy = LIMIT; // prevent overflow
 		}
 		return true;
 	}
 	
 	int find(vector<int>& arr) {
+		int mx = *max_element(arr.begin(), arr.end());
+		
 		int low = 0;
-		int high = *max_element(arr.begin(), arr.end());
+		int high = mx;
 		
 		while (low <= high) {
 			int mid = low + (high - low) / 2;
 			
-			if (check(mid, arr))
+			if (check(mid, arr, mx))
 				high = mid - 1;
 			else
 				low = mid + 1;
